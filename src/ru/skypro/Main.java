70174a46dc51903e1dcd64e7c1cd1d012de379a5
Андрей <79538233369@ya.ru>
2022-04-20 22:54:06 +0500

    3.5

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,21 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Homework 3.4");
+        System.out.println("Homework 3.5");
 
-        byte loseWeight = 7;
-        float loseWeightDayMin = 0.25F;
-        float loseWeightDayMax = 0.5F;
+        int wageMasha = 67760;
+        int wageDenis = 83690;
+        int wageKris = 76230;
 
-        float daysMin = loseWeight / loseWeightDayMin;
-        float daysMax = loseWeight / loseWeightDayMax;
+        int wageMasha10PercentMore = wageMasha + wageMasha/10;
+        int wageDenis10PercentMore = wageDenis + wageDenis/10;
+        int wageKris10PercentMore = wageKris + wageKris/10;
 
-        float daysSummary = daysMax + daysMin;
+        int wageMashaYear = wageMasha * 12;
+        int wageDenisYear = wageDenis * 12;
+        int wageKrisYear = wageKris * 12;
 
-        float daysAverage = daysSummary / 2;
-        float remainderOfDivisionDaysAverage = daysAverage % loseWeight;
-        System.out.println(remainderOfDivisionDaysAverage);
-        System.out.println("It's necessary " + daysAverage + " days in average to loose 7 kg!");
+        int wageMasha10PercentMoreYear = wageMasha10PercentMore * 12;
+        int wageDenis10PercentMoreYear = wageDenis10PercentMore * 12;
+        int wageKris10PercentMoreYear = wageKris10PercentMore * 12;
 
+        int differenceWageMasha = wageMasha10PercentMoreYear - wageMashaYear;
+        int differenceWageDenis = wageDenis10PercentMoreYear - wageDenisYear;
+        int differenceWageKris = wageKris10PercentMoreYear - wageKrisYear;
+
+        System.out.println("Now Masha gets " + wageMasha10PercentMore + " rub. " +
+                "Annual income increased by " + differenceWageMasha + " rub.");
+        System.out.println("Now Denis gets " + wageDenis10PercentMore + " rub. " +
+                "Annual income increased by " + differenceWageDenis + " rub.");
+        System.out.println("Now Kristina gets " + wageKris10PercentMore + " rub. " +
+                "Annual income increased by " + differenceWageKris + " rub.");
     }
 }
