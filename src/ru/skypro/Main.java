4fddcf6fa317ba294fc3c8fd54bd910dc73122ea
Андрей <79538233369@ya.ru>
2022-04-20 21:48:46 +0500

    3.2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,30 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Integer variables");
+        System.out.println("Homework 3.2");
+        float BoxerWeight1 = 78.2F;
+        float BoxerWeight2 = 82.7F;
+        float totalWeight = BoxerWeight1 + BoxerWeight2;
+        System.out.println("The total weight of the boxers is " + totalWeight + "kg");
 
-        byte plusByte = 127;
-        byte minusByte = -128;
-        short plusShort = 32767;
-        short minusShort = -32768;
-        int plusInt = 2147483647;
-        int minusInt = -2147483648;
-        long plusLong = 9223372036854775807l;
-        long minusLong = -9223372036854775808l;
-
-        System.out.println("Floating point variables");
-
-        float plusFloat = 3.44438f;
-        float minusFloat = -3.44438f;
-        double plusDouble = 1.777777;
-        double minusDouble = -1.777777;
-
-        System.out.println("Symbol variables");
-        char zeroChar = 0;
-        char plusChar = 65535;
-
-        System.out.println("Logical variables");
-        boolean Boolean = true || false;
-
+        float differenceWeights = BoxerWeight2 - BoxerWeight1;
+        System.out.println("The difference between the boxers' weights is " + differenceWeights + "kg");
     }
 }
